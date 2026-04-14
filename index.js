ad1f4db5ff8fb8d7d517816567d49ed3bba605bb
私•Λρεχ乂Ιηfιηιτy•ㇱ <apexinfinity243@gmail.com>
2026-04-14 05:52:34 +0100

    Connect to MongoDB and update test route response

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.json());
+
+// 🔌 CONNECT MONGODB
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("✅ MongoDB connecté"))
+.catch(err => console.log(err));
+
+// TEST ROUTE
 app.get("/", (req, res) => {
-    res.send("🚀 Arkanet backend actif");
+    res.send("🚀 Arkanet backend + MongoDB OK");
 });
 
 app.listen(3000, () => {
